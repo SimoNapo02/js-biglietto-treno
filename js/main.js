4a1d7c9845ea1a533b67caadedfefe11ab73aba6
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-04-28 17:06:40 +0200

    correzzione errori js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-// chiedo quanti chilometri si vogliono percorrere
-const Km = Number(prompt ("Quanti chilometri è lungo il tragitto?"));
-// chiedi l'età
-const anni = Number(prompt( "Quanti anni hai?"))
-// calcolo il prezzo del biglietto
-let prezzo = Km * 0.21
-console.log(prezzo)
-// se il passeggero ha meno di 18 anni, sconto 20%
-// se il passeggero ha più di 65 anni, sconto 60%
-// se il passeggero ha tra i 18 e 65 anni, nessuno sconto
+// Prezzo Biglietto: 0.21€ per KM
+const Km = 0.21
+
+
+// 1. Chiedere numero di chilometri da percorrere
+const distanza = Number(prompt("Inserire il numero di chilometri che si vuole percorrere"))
+// 2. Chiedere l'età 
+const anni = Number(prompt("Inserire l'età del passeggero"))
+// 3. Calcolare il prezzo del viaggio
+const prezzo = distanza * Km
+
+document.getElementById("prezzo").innerHTML = prezzo.toFixed(2) + '€'
+// 4. Scontare il biglietto in base all'età
+let finale;
+
+if(anni < 18){
+    finale = (prezzo - ( prezzo * 20 / 100))
+}else if(anni >= 65 ){
+    finale = (prezzo - ( prezzo * 40 / 100))
+}else{
+    finale = prezzo
+}
+
+document.getElementById("finale").innerHTML = finale.toFixed(2) + '€'
+console.log(finale.toFixed(2),"€")
